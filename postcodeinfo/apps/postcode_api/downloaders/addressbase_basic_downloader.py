b7f281ec927fbd5c3218083191ac9d045436a9e9
Al Davidson <apdavidson@gmail.com>
2015-05-19 16:03:42 +0100

    PEP8 compliance - mostly whitespace and line length

--- postcodeinfo/apps/postcode_api/downloaders/addressbase_basic_downloader.py
+++ postcodeinfo/apps/postcode_api/downloaders/addressbase_basic_downloader.py
@@ -3,33 +3,35 @@ import os
 from postcode_api.downloaders.download_manager import DownloadManager
 from postcode_api.downloaders.ftp_download_manager import FTPDownloadManager
 
+
 class AddressBaseBasicDownloader(object):
 
-  def __init__(self, dl_mgr=FTPDownloadManager()):
-    self.download_manager = dl_mgr
+    def __init__(self, dl_mgr=FTPDownloadManager()):
+        self.download_manager = dl_mgr
 
-  def download(self, local_dir='/tmp/', force=False):
+    def download(self, local_dir='/tmp/', force=False):
 
-    self.download_manager.open('osmmftp.os.uk', self.__username(), self.__password())
-    self.download_manager.ftp_client.cwd(self.__source_dir())
+        self.download_manager.open(
+            'osmmftp.os.uk', self.__username(), self.__password())
+        self.download_manager.ftp_client.cwd(self.__source_dir())
 
-    return self.download_manager.download_all_if_needed('./*_csv.zip', local_dir, force)
+        return self.download_manager.download_all_if_needed('./*_csv.zip', local_dir, force)
 
-  def __username(self):
-    username = os.environ.get('OS_FTP_USERNAME')
-    if not username:
-      print 'OS_FTP_USERNAME not set!'
+    def __username(self):
+        username = os.environ.get('OS_FTP_USERNAME')
+        if not username:
+            print 'OS_FTP_USERNAME not set!'
 
-    return username
+        return username
 
-  def __password(self):
-    pwd = os.environ.get('OS_FTP_PASSWORD')
-    if not pwd:
-      print 'OS_FTP_PASSWORD not set!'
+    def __password(self):
+        pwd = os.environ.get('OS_FTP_PASSWORD')
+        if not pwd:
+            print 'OS_FTP_PASSWORD not set!'
 
-    return pwd
-    
-  def __source_dir(self):
-    # TODO - this is currently just the order number.
-    # Make this dynamic based on the most recent directory?
-    return os.environ.get('OS_FTP_ORDER_DIR', '../from-os/DCS0001654526/')
+        return pwd
+
+    def __source_dir(self):
+        # TODO - this is currently just the order number.
+        # Make this dynamic based on the most recent directory?
+        return os.environ.get('OS_FTP_ORDER_DIR', '../from-os/DCS0001654526/')
--- postcodeinfo/apps/postcode_api/downloaders/ftp_download_manager.py
+++ postcodeinfo/apps/postcode_api/downloaders/ftp_download_manager.py
@@ -6,87 +6,88 @@ from ftplib import FTP
 
 from postcode_api.downloaders.download_manager import DownloadManager
 
+
 class FTPDownloadManager(DownloadManager):
 
-  def __init__(self):
-    self.ftp_client = None;
+    def __init__(self):
+        self.ftp_client = None
 
-  def open(self, host, username, password):
-    if self.ftp_client:
-      self.ftp_client.close()
-    
-    self.ftp_client = self.ftp_login(host,username,password)
-    return self.ftp_client
+    def open(self, host, username, password):
+        if self.ftp_client:
+            self.ftp_client.close()
 
-  def ftp_login(self, host, username, password):
-    ftp = ftplib.FTP(host)
-    ftp.login(username, password)
-    return ftp
+        self.ftp_client = self.ftp_login(host, username, password)
+        return self.ftp_client
 
-  def list_files(self, pattern):
-    """ pattern to match against files (e.g. subdir/*.csv) or '.' """
-    headers = self.get_headers(pattern)
-    return map( lambda f: f['url'], headers )
+    def ftp_login(self, host, username, password):
+        ftp = ftplib.FTP(host)
+        ftp.login(username, password)
+        return ftp
 
-  def download_all_if_needed(self, pattern, dirpath, force=False):
-    files = self.list_files(pattern)
-    downloads = []
-    print '%i files matching %s' % (len(files), pattern)
-    for file in files:
-      dl = self.download_if_needed(file, dirpath, force)
-      if dl:
-        downloads.append(dl)
+    def list_files(self, pattern):
+        """ pattern to match against files (e.g. subdir/*.csv) or '.' """
+        headers = self.get_headers(pattern)
+        return map(lambda f: f['url'], headers)
 
-    return "\n".join( downloads )
+    def download_all_if_needed(self, pattern, dirpath, force=False):
+        files = self.list_files(pattern)
+        downloads = []
+        print '%i files matching %s' % (len(files), pattern)
+        for file in files:
+            dl = self.download_if_needed(file, dirpath, force)
+            if dl:
+                downloads.append(dl)
 
-  def get_headers(self, pattern=None):
-    file_list = self.files_in_dir(pattern)
-    headers = map(lambda f: self.interpret_ls_line(f), file_list)
-    return headers
+        return "\n".join(downloads)
 
-  def files_in_dir(self, pattern=None):
-    files = []
-    self.ftp_client.dir(pattern, lambda f: files.append(f))
-    return files
+    def get_headers(self, pattern=None):
+        file_list = self.files_in_dir(pattern)
+        headers = map(lambda f: self.interpret_ls_line(f), file_list)
+        return headers
 
-  def interpret_ls_line(self, line):
-    """ NOTE: this is VERY brittle, but the OS FTP server doesn't
-        support the MSLD command (which is intended to produce
-        machine-readable output) so we don't have many other options.
-        Here we're trying to emulate as closely as possible the headers
-        that we would get back from a HTTP HEAD request
-    """
-    cols = re.split(' +', line)
+    def files_in_dir(self, pattern=None):
+        files = []
+        self.ftp_client.dir(pattern, lambda f: files.append(f))
+        return files
 
-    dic = {
-      'permissions': cols[0],
-      'content-length': int(cols[4]),
-      'last-modified': ' '.join(cols[5:8]),
-      'url': cols[-1],
-      'etag': None
-    }
-    return dic
+    def interpret_ls_line(self, line):
+        """ NOTE: this is VERY brittle, but the OS FTP server doesn't
+            support the MSLD command (which is intended to produce
+            machine-readable output) so we don't have many other options.
+            Here we're trying to emulate as closely as possible the headers
+            that we would get back from a HTTP HEAD request
+        """
+        cols = re.split(' +', line)
 
-  def download_to_dir(self, url, dirpath, headers):
-    filepath = self.filename(dirpath, url)
-    print 'downloading file from ' + url + ' to ' + filepath
+        dic = {
+            'permissions': cols[0],
+            'content-length': int(cols[4]),
+            'last-modified': ' '.join(cols[5:8]),
+            'url': cols[-1],
+            'etag': None
+        }
+        return dic
 
-    file = open(filepath, 'wb')
-    counts = { 'size': 0, 'chunks': 0 }
+    def download_to_dir(self, url, dirpath, headers):
+        filepath = self.filename(dirpath, url)
+        print 'downloading file from ' + url + ' to ' + filepath
 
-    # defined as a local closure so that it can access the variables in local scope
-    # We have to do it this way because:
-    # a) the ftplib interface only supports one argument to the named callback 
-    # b) you can't reassign a primitive inside a closure, but you *can* 
-    #    mutate a key in a dict. This is .... not pleasant, but it works.
-    def handle_chunk_callback(data):
-      counts['size'] += len(data)
-      counts['chunks'] += 1
-      file.write(data)
-      if counts['chunks'] % 100 == 0:
-        print '%i/%i bytes' % (counts['size'], headers['content-length'])
-    
-    self.ftp_client.retrbinary( 'RETR %s' % url, handle_chunk_callback )
+        file = open(filepath, 'wb')
+        counts = {'size': 0, 'chunks': 0}
 
-    print "downloaded to " + filepath
-    return filepath
+        # defined as a local closure so that it can access the variables in local scope
+        # We have to do it this way because:
+        # a) the ftplib interface only supports one argument to the named callback
+        # b) you can't reassign a primitive inside a closure, but you *can*
+        #    mutate a key in a dict. This is .... not pleasant, but it works.
+        def handle_chunk_callback(data):
+            counts['size'] += len(data)
+            counts['chunks'] += 1
+            file.write(data)
+            if counts['chunks'] % 100 == 0:
+                print '%i/%i bytes' % (counts['size'], headers['content-length'])
+
+        self.ftp_client.retrbinary('RETR %s' % url, handle_chunk_callback)
+
+        print "downloaded to " + filepath
+        return filepath
