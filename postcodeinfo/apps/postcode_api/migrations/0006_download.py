da47b31ed7882fadaa61e92f13a9f932fefeaffc
Al Davidson <apdavidson@gmail.com>
2015-05-20 13:58:00 +0100

    pep8-compliance for migrations

--- postcodeinfo/apps/postcode_api/migrations/0006_download.py
+++ postcodeinfo/apps/postcode_api/migrations/0006_download.py
@@ -14,11 +14,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Download',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(
+                    verbose_name='ID', serialize=False,
+                    auto_created=True, primary_key=True)),
                 ('url', models.CharField(max_length=2048, db_index=True)),
-                ('last_modified', models.DateTimeField(auto_now=True, db_index=True)),
+                ('last_modified', models.DateTimeField(
+                    auto_now=True, db_index=True)),
                 ('etag', models.CharField(max_length=2048, db_index=True)),
-                ('local_filepath', models.CharField(max_length=2048, db_index=True)),
+                ('local_filepath', models.CharField(
+                    max_length=2048, db_index=True)),
                 ('state', models.CharField(max_length=8, db_index=True)),
                 ('last_state_change', models.DateTimeField(auto_now=True)),
             ],
