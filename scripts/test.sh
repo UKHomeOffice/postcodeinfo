2e60e14e4aebd3c3ced304eba03760124055f8d2
Ciprian Ciubotariu <ciprian.ciubotariu@digital.justice.gov.uk>
2015-05-29 11:08:29 +0100

    print after the xml report is generated

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,6 +8,10 @@ coverage run --source='.' ./manage.py test
 PARMS=--omit='*migrations*'
 echo "Coverage results:"
 coverage report $PARMS
+
 coverage html $PARMS
-coverage xml -o htmlcov/coverage.xml $PARMS
 echo "HTML report generated in htmlcov/index.html"
+
+coverage xml -o htmlcov/coverage.xml $PARMS
+echo "XML report generated in htmlcov/coverage.xml"
+
