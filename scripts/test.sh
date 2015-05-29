2a766f44ca8de9f374440fcaaab56c4ae6c180b7
Ciprian Ciubotariu <ciprian.ciubotariu@digital.justice.gov.uk>
2015-05-29 11:24:15 +0100

    Remove the html report as the XML one gives us what we need

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,9 +9,6 @@ PARMS=--omit='*migrations*'
 echo "Coverage results:"
 coverage report $PARMS
 
-coverage html $PARMS
-echo "HTML report generated in htmlcov/index.html"
-
 coverage xml -o htmlcov/coverage.xml $PARMS
 echo "XML report generated in htmlcov/coverage.xml"
 
