0c31890032d0815a67bec4c07b71169e035937d2
Al Davidson <apdavidson@gmail.com>
2015-06-01 16:54:04 +0100

    pep8-compliance and more pythonic structure

--- postcodeinfo/apps/postcode_api/migrations/0010_auto_20150601_1513.py
+++ postcodeinfo/apps/postcode_api/migrations/0010_auto_20150601_1513.py
@@ -8,22 +8,31 @@ from django.db import models, migrations
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
+
 def create_super_user(apps, schema_editor):
-    username = os.environ.get('PCI_DEFAULT_ADMIN_USER_USERNAME')
-    email = os.environ.get('PCI_DEFAULT_ADMIN_USER_EMAIL')
-    password = os.environ.get('PCI_DEFAULT_ADMIN_USER_PASSWORD')
-    
+    username = os.environ.get('ADMIN_USER_USERNAME')
+    email = os.environ.get('ADMIN_USER_EMAIL')
+    password = os.environ.get('ADMIN_USER_PASSWORD')
+
     existing_users = User.objects.filter(username=username)
 
-    if username == None:
-      print 'skipping admin user creation - no defaults set (PCI_DEFAULT_ADMIN_USER_xyz)'
+    if not existing_users:
+        print (
+            'skipping admin user creation - '
+            'no defaults set (ADMIN_USER_xyz)'
+            )
     else:
-      if len(existing_users) == 0:
-        user = User.objects.create_superuser(username, email, password)
-        auth_token = Token.objects.create(user=user)
-        print 'created superuser with auth_token %s' % str(auth_token)
-      else:
-        print 'user with username %s already exists' % username
+        if len(existing_users) == 0:
+            user = User.objects.create_superuser(username, email, password)
+            auth_token = Token.objects.create(user=user)
+            print 'created superuser with auth_token {token}'.format(
+                token=str(auth_token))
+        else:
+            print (
+                'user with username {username} already exists'
+                ' (auth token {token})').format(
+                    username=username,
+                    token=existing_users.first().auth_token)
 
 
 class Migration(migrations.Migration):
@@ -33,5 +42,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-      migrations.RunPython(create_super_user)
+        migrations.RunPython(create_super_user)
     ]
