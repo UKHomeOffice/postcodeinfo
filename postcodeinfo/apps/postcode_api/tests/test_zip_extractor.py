625a4c27ae96e0b0caa83e9984d240fe673fa6cd
Al Davidson <apdavidson@gmail.com>
2015-06-23 11:07:07 +0100

    got rid of decorator-style patches and restructured with setUp/tearDown methods for clarity

--- postcodeinfo/apps/postcode_api/tests/test_zip_extractor.py
+++ postcodeinfo/apps/postcode_api/tests/test_zip_extractor.py
@@ -7,18 +7,19 @@ from postcode_api.utils import ZipExtractor
 
 class ZipExtractorTestCase(TestCase):
 
-    @patch('zipfile.is_zipfile', return_value=True)
-    @patch('postcode_api.utils.ZipExtractor.unzip')
-    def test_that_when_given_a_zip_file_it_unzips_it(self,
-                                                     mock_unzip,
-                                                     mock_zip_file):
-        ZipExtractor('/my/test/file').unzip_if_needed('*')
-        mock_unzip.assertCalledWith('/my/test/file')
+    def setUp(self):
+        self.patch_unzip = patch('postcode_api.utils.ZipExtractor.unzip')
+        self.mock_unzip = self.patch_unzip.start()
 
-    @patch('zipfile.is_zipfile', return_value=False)
-    @patch('postcode_api.utils.ZipExtractor.unzip')
-    def test_that_when_not_given_a_zip_file_it_does_not_unzip(self,
-                                                              mock_unzip,
-                                                              mock_zip_file):
-        rtn = ZipExtractor('/my/test/file').unzip_if_needed('*')
-        assert not mock_unzip.called
+    def tearDown(self):
+        self.patch_unzip.stop()
+
+    def test_that_when_given_a_zip_file_it_unzips_it(self):
+        with patch('zipfile.is_zipfile', return_value=True):
+            ZipExtractor('/my/test/file').unzip_if_needed('*')
+            self.mock_unzip.assertCalledWith('/my/test/file')
+
+    def test_that_when_not_given_a_zip_file_it_does_not_unzip(self):
+        with patch('zipfile.is_zipfile', return_value=False):
+            rtn = ZipExtractor('/my/test/file').unzip_if_needed('*')
+            assert not self.mock_unzip.called
