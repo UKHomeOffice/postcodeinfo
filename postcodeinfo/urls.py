a3f311ca54505b399066219dcbb787252050ffcb
Al Davidson <apdavidson@gmail.com>
2015-06-23 11:42:11 +0100

    took out unused imports

--- postcodeinfo/urls.py
+++ postcodeinfo/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from rest_framework import routers
-from rest_framework.urlpatterns import format_suffix_patterns
 
 from postcode_api import views
 
