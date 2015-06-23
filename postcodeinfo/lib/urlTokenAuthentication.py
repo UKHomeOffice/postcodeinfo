e30242f8e234546a0efd50ed5eaf090f9fcf7626
Al Davidson <apdavidson@gmail.com>
2015-06-23 11:41:50 +0100

    fixed

--- postcodeinfo/lib/urlTokenAuthentication.py
+++ postcodeinfo/lib/urlTokenAuthentication.py
@@ -4,7 +4,6 @@ from rest_framework.authentication import TokenAuthentication
 class UrlTokenAuthentication(TokenAuthentication):
 
     def authenticate(self, request):
-        token = None
         if 'token' in request.GET:
             token = request.GET['token']
             return self.authenticate_credentials(token)
