83ec0a326582dea548c2cf2cc5c2ee785d4fcda1
Al Davidson <apdavidson@gmail.com>
2015-06-29 17:20:52 +0100

    removed spaces in brackets

--- postcodeinfo/apps/postcode_api/importers/addressbase_basic_importer.py
+++ postcodeinfo/apps/postcode_api/importers/addressbase_basic_importer.py
@@ -35,7 +35,8 @@ def batch(iterable, size):
         chunk = itertools.islice(it, size)
         yield itertools.chain((next(chunk),), chunk)
 
-def _get_all_uprns(batch_list):
+
+def get_all_uprns(batch_list):
     return [row['uprn'] for row in batch_list if row]
 
 
@@ -77,58 +78,64 @@ class AddressBaseBasicImporter(object):
 
     def _append(self, change_type, address):
         collection = {
-         'I': self.inserts,
-         'U': self.updates,
-         'D': self.deletes}[change_type]
+            'I': self.inserts,
+            'U': self.updates,
+            'D': self.deletes}[change_type]
 
         collection.append(address)
 
     def import_csv(self, filename):
         total_rows = lines_in_file(filename)
-        batch_size = int(os.environ.get( 'BATCH_IMPORT_NUM_ROWS', (total_rows // 10) or 1 ))
-        bulk_create_batch_size = int(os.environ.get('BULK_CREATE_BATCH_SIZE', 1000))
+        batch_size = int(
+            os.environ.get('BATCH_IMPORT_NUM_ROWS', (total_rows // 10) or 1))
+        bulk_create_batch_size = int(
+            os.environ.get('BULK_CREATE_BATCH_SIZE', 1000))
 
         rows = csv_rows(filename, fieldnames=self.fieldnames)
 
-        logging.debug( 'reading all data and getting uprns' )
-        logging.debug( 'getting batches of size {i}'.format(i=batch_size) )
+        logging.debug('reading all data and getting uprns')
+        logging.debug('getting batches of size {i}'.format(i=batch_size))
         batches = batch(rows, batch_size)
-        
+
         for current_batch in batches:
             batch_list = list(current_batch)
             logging.debug('**** starting batch ****')
-            uprns = _get_all_uprns(batch_list)
+            uprns = get_all_uprns(batch_list)
 
-            logging.debug( 'looking for existing addresses with uprns in this batch' )
-            existing_address_dict = self._get_existing_addresses_in_batch_as_dict(uprns)
+            logging.debug(
+                'looking for existing addresses with uprns in this batch')
+            existing_address_dict = self._get_existing_addresses_in_batch_as_dict(
+                uprns)
 
             logging.debug('constructing model objects')
-            self._construct_model_objects( batch_list, existing_address_dict, total_rows)
+            self._construct_model_objects(
+                batch_list, existing_address_dict, total_rows)
 
             logging.debug('**** saving ****')
             self._save(bulk_create_batch_size)
 
             logging.debug('batch done')
 
-
     def _get_existing_addresses_in_batch_as_dict(self, uprns):
-        logging.debug( 'querying db for existing uprns')
+        logging.debug('querying db for existing uprns')
         existing_addresses = Address.objects.filter(uprn__in=uprns)
-            
-        logging.debug( 'building hash' )
-        return dict ((o.uprn, o) for o in existing_addresses)
+
+        logging.debug('building hash')
+        return dict((o.uprn, o) for o in existing_addresses)
 
     def _construct_model_objects(self, batch_list, existing_address_dict, total_rows):
         with ImporterProgress(total_rows) as progress:
             for row in batch_list:
                 if row:
-                    address = self._existing_or_new_address(row, existing_address_dict)
+                    address = self._existing_or_new_address(
+                        row, existing_address_dict)
                     address = self._apply_changes_to_instance(address, row)
                     self._append(row['change_type'], address)
                     progress.increment(row['uprn'])
 
     def _save(self, bulk_create_batch_size):
-        logging.debug('bulk_create_batch_size = {batch_size}'.format(batch_size=bulk_create_batch_size))
+        logging.debug('bulk_create_batch_size = {batch_size}'.format(
+            batch_size=bulk_create_batch_size))
 
         to_delete = [obj.pk for obj in self.updates + self.deletes]
 
@@ -138,10 +145,12 @@ class AddressBaseBasicImporter(object):
 
             # delete inserts which are already in the db and re-insert them
             inserts = [obj.pk for obj in self.inserts]
-            logging.debug('deleting {num} existing addresses to insert in batches of {batch_size}'.format(num=len(inserts), batch_size=bulk_create_batch_size))
+            logging.debug('deleting {num} existing addresses to insert in batches of {batch_size}'.format(
+                num=len(inserts), batch_size=bulk_create_batch_size))
             Address.objects.filter(pk__in=inserts).delete()
 
-            logging.debug('bulk_creating {num} updates in batches of {batch_size}'.format(num=len(inserts), batch_size=bulk_create_batch_size))
+            logging.debug('bulk_creating {num} updates in batches of {batch_size}'.format(
+                num=len(inserts), batch_size=bulk_create_batch_size))
             Address.objects.bulk_create(self.updates, bulk_create_batch_size)
 
             logging.debug('bulk_creating %i inserts' % len(self.inserts))
