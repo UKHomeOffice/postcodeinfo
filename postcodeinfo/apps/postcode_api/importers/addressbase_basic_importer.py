ff94ab04e84c0fb1fd02a71924fee2e79cc7ad83
Al Davidson <apdavidson@gmail.com>
2015-07-06 10:36:49 +0100

    changed importer to use original filename as prefix for splitdir

--- postcodeinfo/apps/postcode_api/importers/addressbase_basic_importer.py
+++ postcodeinfo/apps/postcode_api/importers/addressbase_basic_importer.py
@@ -13,7 +13,7 @@ BRITISH_NATIONAL_GRID = 27700
 def split_file(path, num_lines):
     filename = os.path.basename(path)
     split_dir = tempfile.mkdtemp(prefix=filename + '-')
-    split_file_prefix = "{split_dir}/"
+    split_file_prefix = split_dir + "/"
     runProcess(
         ['/usr/bin/split', '-l', str(num_lines), path, split_file_prefix])
 
