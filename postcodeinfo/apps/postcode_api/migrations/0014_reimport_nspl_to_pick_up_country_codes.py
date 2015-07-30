0462b5fed8bd12cca722f0224f95cc4fbc067d4a
Al Davidson <apdavidson@gmail.com>
2015-07-30 15:22:14 +0100

    added migration to re-import NSPL, in order to pick up the country codes added in the last migration

---
+++ postcodeinfo/apps/postcode_api/migrations/0014_reimport_nspl_to_pick_up_country_codes.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+
+from django.db import models, migrations
+from django.conf import settings
+
+from postcode_api.downloaders.postcode_gss_code import PostcodeGssCodeDownloader
+from postcode_api.importers.postcode_gss_code_importer import PostcodeGssCodeImporter
+from postcode_api.utils import ZipExtractor
+
+def rerun_nspl_import(apps, schema_editor):
+  if settings.DEBUG:
+      print 'running in debug - skipping re-import of NSPL'
+  else:
+    destination_dir = dest_dir('/tmp/postcode_gss_codes/')
+    filepath = PostcodeGssCodeDownloader().download(destination_dir)
+    if isinstance(filepath, list):
+        filepath = filepath[0]
+        
+    files = ZipExtractor(filepath).unzip_if_needed('.*NSPL.*\.csv')
+    for path in files:
+        print 'importing ' + path
+        PostcodeGssCodeImporter().import_postcode_gss_codes(path)
+
+
+def dest_dir(path):
+  if not os.path.exists(path):
+    os.makedirs(path)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('postcode_api', '0013_populate_countries'),
+    ]
+
+    operations = [
+      migrations.RunPython(rerun_nspl_import)
+    ]
