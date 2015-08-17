cbb5948e6a3f3cafd5bd11d52ebe05a53824441b
Al Davidson <apdavidson@gmail.com>
2015-08-17 14:06:04 +0100

    pep-8 style issues

--- postcodeinfo/apps/postcode_api/tests/importers/test_local_authorities_importer.py
+++ postcodeinfo/apps/postcode_api/tests/importers/test_local_authorities_importer.py
@@ -3,14 +3,18 @@ import os
 from django.test import TestCase
 
 from postcode_api.models import LocalAuthority
-from postcode_api.importers.local_authorities_importer import LocalAuthoritiesImporter
+from postcode_api.importers.local_authorities_importer \
+    import LocalAuthoritiesImporter
 
-ROWS_IN_SAMPLE_DATA_FILE = 7;
+
+ROWS_IN_SAMPLE_DATA_FILE = 7
+
 
 class LocalAuthoritiesImporterTestCase(TestCase):
 
     def _sample_data_file(self, filename):
-        return os.path.join(os.path.dirname(__file__), '../', 'sample_data/', filename)
+        return os.path.join(os.path.dirname(__file__), '../',
+                            'sample_data/', filename)
 
     def _importer(self):
         return LocalAuthoritiesImporter()
@@ -29,13 +33,19 @@ class LocalAuthoritiesImporterTestCase(TestCase):
         initial_la_count = LocalAuthority.objects.count()
         self._import_data_from('local_authorities_sample.nt')
         # expectation
-        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
+        self.assertEqual(
+            LocalAuthority.objects.count(),
+            initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
 
     def test_that_when_existing_local_authorities_are_imported_then_duplicate_local_authority_records_dont_get_created(self):
         # setup
         initial_la_count = LocalAuthority.objects.count()
         self._import_data_from('local_authorities_sample.nt')
-        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
+        self.assertEqual(
+            LocalAuthority.objects.count(),
+            initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
         self._import_data_from('local_authorities_sample.nt')
         # expectation
-        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
+        self.assertEqual(
+            LocalAuthority.objects.count(),
+            initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
