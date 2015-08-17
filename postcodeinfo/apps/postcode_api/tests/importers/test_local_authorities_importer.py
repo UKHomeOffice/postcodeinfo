b8465dd17a29821fbe85b86efb837f97ff6feb73
Al Davidson <apdavidson@gmail.com>
2015-08-17 13:38:06 +0100

    replaced hard-coded 7 with ROWS_IN_SAMPLE_DATA_FILE

--- postcodeinfo/apps/postcode_api/tests/importers/test_local_authorities_importer.py
+++ postcodeinfo/apps/postcode_api/tests/importers/test_local_authorities_importer.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from postcode_api.models import LocalAuthority
 from postcode_api.importers.local_authorities_importer import LocalAuthoritiesImporter
 
+ROWS_IN_SAMPLE_DATA_FILE = 7;
 
 class LocalAuthoritiesImporterTestCase(TestCase):
 
@@ -28,13 +29,13 @@ class LocalAuthoritiesImporterTestCase(TestCase):
         initial_la_count = LocalAuthority.objects.count()
         self._import_data_from('local_authorities_sample.nt')
         # expectation
-        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + 7)
+        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
 
     def test_that_when_existing_local_authorities_are_imported_then_duplicate_local_authority_records_dont_get_created(self):
         # setup
         initial_la_count = LocalAuthority.objects.count()
         self._import_data_from('local_authorities_sample.nt')
-        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + 7)
+        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
         self._import_data_from('local_authorities_sample.nt')
         # expectation
-        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + 7)
+        self.assertEqual(LocalAuthority.objects.count(), initial_la_count + ROWS_IN_SAMPLE_DATA_FILE)
