8b2df80e3a6c6eff3527a5d7a1ab4db8e2905dc4
Al Davidson <apdavidson@gmail.com>
2015-08-20 17:24:47 +0100

    refactored the FTP-specific code onto the ftp downloader, and added tests

--- postcodeinfo/apps/postcode_api/downloaders/addressbase_basic.py
+++ postcodeinfo/apps/postcode_api/downloaders/addressbase_basic.py
@@ -4,6 +4,7 @@ AddressBase Basic downloader class
 """
 
 import logging
+import ftplib
 import os
 
 from .filesystem import LocalCache
@@ -15,6 +16,7 @@ log = logging.getLogger(__name__)
 
 
 class AddressBaseBasicDownloader(LocalCache, S3Cache, FtpDownloader):
+
     """
     Ordnance Survey remove the files from the download directory after 21 days,
     so we cache the files on Amazon S3 in case we need them after that time.
@@ -31,8 +33,7 @@ class AddressBaseBasicDownloader(LocalCache, S3Cache, FtpDownloader):
             'osmmftp.os.uk',
             os.environ.get('OS_FTP_USERNAME'),
             os.environ.get('OS_FTP_PASSWORD'),
-            path=os.environ.get(
-                'OS_FTP_ORDER_DIR', '../from-os/DCS0001654526/'))
+            path=self.find_dir_with_latest_full_file())
 
     def download(self, dest_dir=None):
         """
@@ -42,3 +43,29 @@ class AddressBaseBasicDownloader(LocalCache, S3Cache, FtpDownloader):
 
         return super(AddressBaseBasicDownloader, self).download(
             '*_csv.zip', dest_dir)
+
+    # Ordnance Survey's update mechanism creates a *new* order number
+    # for every update, so we cannot predict ahead of time what the
+    # directory path will be.
+    # So we work it out as follows:
+    # - the files are all called AddressBase_FULL_YYYY-MM-DD_NNN_csv.zip
+    # - get ALL the files matching that pattern in all the subidrectories
+    # - split into path / filename
+    # - sort by filename
+    # - the last file should be the latest, so use the directory containing it
+    def find_dir_with_latest_full_file(self):
+        # have to create new object rather than exploiting the
+        # inheritance heirarchy, as this method is called during
+        # initialisation
+        tmp_ftp = FtpDownloader('osmmftp.os.uk',
+            os.environ.get('OS_FTP_USERNAME'),
+            os.environ.get('OS_FTP_PASSWORD'),
+            '../from-os/')
+        # full_files = tmp_ftp._list('*/AddressBase_FULL_*')
+        # parsed_file_list = map(lambda fname: {'dir': fname.split(
+        #     '/')[0], 'file': fname.split('/')[-1]}, full_files)
+        # latest = sorted(parsed_file_list, key=lambda key: key['file'])[-1]
+
+        latest = tmp_ftp.find_dir_with_latest_file_matching('*/AddressBase_FULL_*')
+        if latest:
+            return latest['dir']
