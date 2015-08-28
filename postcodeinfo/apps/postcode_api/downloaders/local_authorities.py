16204fc75bc6ab61a4656dc88a36b861936a4167
Al Davidson <apdavidson@gmail.com>
2015-08-28 14:36:13 +0100

    Fixed "KeyError: 'last-modified'" when downloading local authorities Caused by DCLG moving their datafile without a redirect. It's now on publishmydata.com, a commercial service that adds more layers of redirect, and doesn't provide a last-modified header.

--- postcodeinfo/apps/postcode_api/downloaders/local_authorities.py
+++ postcodeinfo/apps/postcode_api/downloaders/local_authorities.py
@@ -3,6 +3,8 @@
 Local Authorities downloader class
 """
 
+import os
+
 from .filesystem import LocalCache
 from .http import HttpDownloader
 from .s3 import S3Cache
@@ -10,6 +12,13 @@ from .s3 import S3Cache
 
 class LocalAuthoritiesDownloader(LocalCache, S3Cache, HttpDownloader):
 
+    def _authoritative_url(self):
+        default_url = 'http://opendatacommunities-graphs.publishmydata.com'\
+                      '?graph-uri=http://opendatacommunities.org/graph/dev'\
+                      '-local-authorities'
+        
+        return os.environ.get('LOCAL_AUTHORITIES_DUMP_URL') or default_url
+
     def __init__(self):
         super(LocalAuthoritiesDownloader, self).__init__(
-            'http://opendatacommunities.org/data/dev-local-authorities/dump')
+            self._authoritative_url())
